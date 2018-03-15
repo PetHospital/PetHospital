ff54e9b06ae72814d645c5cd7bd59fd7c5cfa8c7
RachelYang2 <lnfsyxr@163.com>
2018-03-15 09:15:30 +0800

    add register form

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../app/login/login.component';
 import { RegisterComponent } from '../app/register/register.component';
+import { SceneComponent } from './scene/scene.component';
 
 
 export const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: 'signup', component: RegisterComponent}
+    { path: 'signup', component: RegisterComponent},
+    { path: 'scene', component: SceneComponent},
 ];
 
 @NgModule({
