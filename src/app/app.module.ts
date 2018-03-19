b52c3aaf6755d53cd66768370373879721a99e9f
Pandaice <1542752218@qq.com>
2018-03-19 13:32:47 +0800

    modify route and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app.routing';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { SceneComponent } from './scene/scene.component';
 import { IndexComponent } from './index/index.component';
-
-import { AppRoutingModule } from './app.routing';
 import { DialogComponent } from './dialog/dialog.component';
 import { BigPicComponent } from './big-pic/big-pic.component';
 import { CardComponent } from './card/card.component';
 import { TutorComponent } from './tutor/tutor.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MedicalRecordComponent } from './medical-record/medical-record.component';
+
 
 @NgModule({
     declarations: [
@@ -19,11 +24,15 @@ import { TutorComponent } from './tutor/tutor.component';
         DialogComponent,
         BigPicComponent,
         CardComponent,
-        TutorComponent
+        TutorComponent,
+        LoginComponent,
+        RegisterComponent,
+        MedicalRecordComponent
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SceneComponent } from './scene/scene.component';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from '../app/login/login.component';
 import { RegisterComponent } from '../app/register/register.component';
+import { SceneComponent } from './scene/scene.component';
+import { TutorComponent } from '../app/tutor/tutor.component';
+import { MedicalRecordComponent } from './medical-record/medical-record.component';
 
 
 export const appRoutes: Routes = [
     { path: '', component: IndexComponent },
-    { path: 'scene', component: SceneComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: RegisterComponent},
     { path: 'scene', component: SceneComponent},
+    { path: 'role', component: TutorComponent },
+    { path: 'disease', component: MedicalRecordComponent}
 ];
 
 @NgModule({
