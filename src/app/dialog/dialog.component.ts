50cb2ac12a35a55ac67e5ed12b883840b4c16c5d
RachelYang2 <lnfsyxr@163.com>
2018-03-22 08:55:29 +0800

    add dialog

--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -9,7 +9,43 @@ export class DialogComponent implements OnInit {
 
   constructor() { }
 
+  progresses = [
+    'first step',
+    'second step',
+    'third step',
+    'fourth step'
+  ];
+
+  userMessage = {
+    "true": "Yes I understand",
+    "false": "Sorry I don't get it"
+  };
+
+  currentIndex = 0;
+  messages = [];
+
   ngOnInit() {
+    this.messages.push({
+      "content": this.progresses[0],
+      "isUser": false
+    });
   }
 
+  addSysMessage = (flag) => {
+    if (flag) {
+      this.currentIndex ++;
+    }
+    this.messages.push({
+      "content": this.progresses[this.currentIndex],
+      "isUser": false
+    });
+  }
+
+  addUserMessage = (flag) => {
+    this.messages.push({
+      "content": this.userMessage[flag],
+      "isUser": true
+    });
+    this.addSysMessage(flag);    
+  }
 }
