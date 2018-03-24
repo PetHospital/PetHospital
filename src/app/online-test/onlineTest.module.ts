03d2ccc459d0a194c4d5c48ccbdeb8d0b457ca79
Tedstack <1072265002@qq.com>
2018-03-24 12:29:42 +0800

    onlineTest part

--- src/app/online-test/onlineTest.module.ts
+++ src/app/online-test/onlineTest.module.ts
@@ -3,7 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ExamComponent } from './exam/exam.component';
 import { PracticeComponent } from './practice/practice.component';
 import { CountdownComponent } from './countdown/countdown.component';
-
+import { PracticeService } from './practice/practice.service';
+import { ExamService } from './exam/exam.service';
 @NgModule({
     imports: [
         CommonModule
@@ -13,6 +14,10 @@ import { CountdownComponent } from './countdown/countdown.component';
         PracticeComponent,
         CountdownComponent
     ],
-    exports: [PracticeComponent]
+    providers: [
+        PracticeService,
+        ExamService
+    ],
+    exports: [ExamComponent]
 })
 export class OnlineTestModule { }
