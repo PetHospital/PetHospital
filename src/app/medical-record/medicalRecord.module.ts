5d70ef64104339841a5e36dc8a5540f4157846a3
Pandaice <1542752218@qq.com>
2018-04-12 10:50:41 +0800

    medical css finish

--- src/app/medical-record/medicalRecord.module.ts
+++ src/app/medical-record/medicalRecord.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MedicalRecordComponent } from './medical-record.component';
 import { BigPicModule } from '../roleplay/big-pic/bigPic.module';
-import { SharedModule } from './../shared/shared.module';
-
+// import { HeaderComponent } from '../shared/component/header/header.component';
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
     imports: [
         CommonModule,
